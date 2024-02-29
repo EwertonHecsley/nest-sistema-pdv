db5f2751752dd981bbefbf8e057788c0375d100f
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-29 10:26:53 -0300

    instancia do PrismaService e dump sql para categorias

---
+++ dump.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    categorias (descricao)
+VALUES ('informatica'),
+    ('celulares'),
+    ('beleza e perfumaria'),
+    ('mercado'),
+    ('livros e papelaria'),
+    ('brinquedos'),
+    ('moda'),
+    ('bebe'),
+    ('games');
