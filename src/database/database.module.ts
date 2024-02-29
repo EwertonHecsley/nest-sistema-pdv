e153dd6e873c16d6cd53da3b9d0fbc5434702d00
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-29 10:19:18 -0300

    Criacao do PrismaService

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma-service/prisma.service';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+    providers: [PrismaService]
+})
+export class DatabaseModule { }
